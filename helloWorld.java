a9c914fc11e97efd276d77facd6d286766b2d917
meow-codes201 <humairat291@gmail.com>
2026-06-21 21:58:55 +0530

    Hello world

--- helloWorld.java
+++ helloWorld.java
@@ -1,6 +1,30 @@
-public class helloWorld{
-    public static void main(){
+public class helloWorld
+{
+    public static void main(String[] args)
+    {
         System.out.println("hello world!");
+        String name = "meow codes";
+        System.out.println("Hello,"+name);
+        System.out.println(10+90);
+        int age = 19;
+        if(age>18){
+            System.out.println("Coding!");
+
+        }
+        else{
+            System.out.println("watching cartoons");
+        }
+
+        int x = 10;
+        int y = 90;
+        System.out.println(x+y);
+        System.out.println(y-x);
+        System.out.println(x*y);
+        System.out.println(y/x);
+
+
+
+
 
     }
 }
