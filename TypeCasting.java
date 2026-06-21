e9ec12da2bd457cc949bfc2645f4e6af4319f1b8
meow-codes201 <humairat291@gmail.com>
2026-06-21 23:22:03 +0530

    Type Casting in Java

---
+++ TypeCasting.java
@@ -0,0 +1,11 @@
+public class TypeCasting{
+    public static void main(String[] args){
+        byte b = 100;
+        int a = 200;
+        b = (byte) a;
+        System.out.println(b);
+
+
+
+    }
+}
