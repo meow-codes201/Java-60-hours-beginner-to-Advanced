160a49f73f2a333f38d6d23b3ab453b24c2bda93
meow-codes201 <humairat291@gmail.com>
2026-06-21 23:21:41 +0530

    Hello world

--- helloWorld.java
+++ helloWorld.java
@@ -22,7 +22,26 @@ public class helloWorld
         System.out.println(x*y);
         System.out.println(y/x);
 
+// data types - primitive & non - primitive 
+// primitive - byte,short, int, long ; float, double; char; boolean
 
+byte b = 120; // 1 byte
+short s = 258; // 2 bytes
+int i = 255; // 4 bytes
+long l = 1080l; //end with L, 8 bytes
+float f= 3.14F; //end with f, 4 bytes
+double d = 3.1459; //8 bytes - default for floating point numbers
+char c = '$'; //2 bytes, unicode
+boolean bi = true; //
+
+int bin = 0b1000;
+int hex = 0xFF;
+double big = 13E9;
+System.out.println(b+" "+s+" "+i+" "+l+" "+f+" "+d+" "+c+" "+bi+" "+bin+" "+hex+" "+big);
+//char 
+char ch = 'a';
+ch++;
+System.out.println(ch);
 
 
 
